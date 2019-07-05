b4402daa18c9e602a04224b265e6b8a5a801782b
ritar <skysoyn@gmail.com>
2019-07-05 17:16:58 +0900

    disabled hover for mobile devices

--- tipe-it-pwa/src/components/Border.vue
+++ tipe-it-pwa/src/components/Border.vue
@@ -1,52 +1,68 @@
 <template>
-<div class="line-wrapper">
-  <div class="line" v-if="contentIndex!=0" v-on:click="add(contentIndex)" 
-      v-bind:id="'border-'+contentIndex">
-    <div class="add-item">+<span class="shortcut-text">⌘ + Enter / Ctrl + Enter</span></div>
+  <div class="line-wrapper">
+    <div
+      class="line"
+      v-if="contentIndex!=0"
+      v-on:click="add(contentIndex)"
+      v-bind:id="'border-'+contentIndex"
+    >
+      <div class="add-item">
+        +
+        <span class="shortcut-text">⌘ + Enter / Ctrl + Enter</span>
+      </div>
+    </div>
+    <div
+      class="line line-top"
+      v-if="contentIndex==0"
+      v-on:click="add(contentIndex)"
+      v-bind:id="'border-'+contentIndex"
+    >
+      <div class="add-item">+</div>
+    </div>
   </div>
-  <div class="line line-top" v-if="contentIndex==0" v-on:click="add(contentIndex)" 
-      v-bind:id="'border-'+contentIndex">
-    <div class="add-item">+</div>
-  </div>
-</div>
 </template>
 
 <script>
-import PropertyStore from '../models/PropertyStore'
-import autosize from 'autosize'
-import { setTimeout } from 'timers'
+import PropertyStore from "../models/PropertyStore";
+import autosize from "autosize";
+import { setTimeout } from "timers";
 
 export default {
-	name: 'Border',
-	props: {
-		contentIndex: {		
-			required: true
-		}
-	},
-	methods: {
-		add: function (place) {
-			// eslint-disable-next-line
-			PropertyStore.state.property.content.splice(place, 0, {text: '', title: ''})
-			this.$nextTick(function () {
-        var thisBorder = document.getElementById('border-'+this.contentIndex)
-        thisBorder.classList.add('none')
-        document.getElementById("texts").scrollTop += 60
-        setTimeout(function(){thisBorder.classList.remove('none')}, 100)
-        document.getElementById('textarea-'+this.contentIndex).focus()
-				// eslint-disable-next-line
-        autosize.update(document.querySelectorAll('textarea'))
-        autosize(document.querySelectorAll('textarea'))
-			})
-		}
-	}
-}
+  name: "Border",
+  props: {
+    contentIndex: {
+      required: true
+    }
+  },
+  methods: {
+    add: function(place) {
+      // eslint-disable-next-line
+      PropertyStore.state.property.content.splice(place, 0, {
+        text: "",
+        title: ""
+      });
+      this.$nextTick(function() {
+        var thisBorder = document.getElementById("border-" + this.contentIndex);
+        thisBorder.classList.add("none");
+        document.getElementById("texts").scrollTop += 60;
+        setTimeout(function() {
+          thisBorder.classList.remove("none");
+        }, 100);
+        document.getElementById("textarea-" + this.contentIndex).focus();
+        // eslint-disable-next-line
+        autosize.update(document.querySelectorAll("textarea"));
+        autosize(document.querySelectorAll("textarea"));
+      });
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
 .none {
   display: none;
   opacity: 0;
-  &:hover{
+  &:hover {
     display: none;
     opacity: 0;
   }
@@ -57,12 +73,13 @@ export default {
   border-bottom: 1px solid;
   border-color: rgba(0, 0, 0, 0.1) transparent transparent transparent;
   display: flex;
-  transition: border-color 0.3s cubic-bezier(0.215, 0.61, 0.355, 1), height 0.3s cubic-bezier(0.215, 0.61, 0.355, 1);
+  transition: border-color 0.3s cubic-bezier(0.215, 0.61, 0.355, 1),
+    height 0.3s cubic-bezier(0.215, 0.61, 0.355, 1);
   transition-delay: 0.1s;
   align-items: center;
   cursor: pointer;
 
-  .add-item{
+  .add-item {
     width: 100%;
     // text-align: center;
     font-size: 20px;
@@ -85,19 +102,21 @@ export default {
     vertical-align: middle;
     text-align: right;
   }
-  &:hover{
-    height: 72px;
-    margin-bottom: 16px;
-    border-color: rgba(0, 0, 0, 0.1) transparent;
-    .add-item{
-      opacity: 1;
+  @media (hover: hover) {
+    &:hover {
+      height: 72px;
+      margin-bottom: 16px;
+      border-color: rgba(0, 0, 0, 0.1) transparent;
+      .add-item {
+        opacity: 1;
+      }
     }
   }
 }
-.line-top{
+.line-top {
   border-top: none !important;
-  &:hover{
-    border-top: none !important;    
+  &:hover {
+    border-top: none !important;
   }
 }
 </style>
